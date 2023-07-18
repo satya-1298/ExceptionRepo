11f4e672a21409aaf7326e1886b101e9ae58f533
unknown <satya-1298>
2023-07-18 09:29:08 +0530

    [Satya]Add : Custom Argument range Exception

---
+++ ExceptionHandle_Demo/IndexOut_Of_Range_Exception.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandle_Demo
+{
+    public class IndexOut_Of_Range_Exception
+    {
+        public void Range()
+        {
+            //try
+            //{
+            //    List<Exception> exceptions = new List<Exception>();
+            //    Console.WriteLine(exceptions[2]);
+            //}
+            //catch (ArgumentOutOfRangeException ex) 
+            //{
+            //    Console.WriteLine(ex.Message);
+            //}
+            try
+            {
+                string str = null;
+                Console.WriteLine(str);
+            }
+            catch(NullReferenceException ex) 
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            
+        }
+    }
+}
--- ExceptionHandle_Demo/Program.cs
+++ ExceptionHandle_Demo/Program.cs
@@ -10,11 +10,40 @@ namespace ExceptionHandle_Demo
     {
         static void Main(string[] args)
         {
-            //HandleException handleException = new HandleException();
-            //handleException.Display();
-            DivideByZero_Exception divideByZero_Exception = new DivideByZero_Exception();
-            divideByZero_Exception.Custom();
-            Console.ReadLine();
+            while(true) 
+            {
+                Console.WriteLine("Choose the below options");
+                Console.WriteLine("1-TryCatch");
+                Console.WriteLine("2-Custom Exception");
+                Console.WriteLine("3-Null Exception");
+                Console.WriteLine("4-Index out of range");
+                Console.WriteLine("-------------------------------");
+                int option =Convert.ToInt32(Console.ReadLine());
+                switch(option)
+                {
+                    case 1:
+                        HandleException handleException = new HandleException();
+                        handleException.Display();
+                        Console.WriteLine("-------------------------------");
+                        break;
+                   case 2:
+                        DivideByZero_Exception divideByZero_Exception = new DivideByZero_Exception();
+                        divideByZero_Exception.Custom();
+                        Console.WriteLine("-------------------------------");
+                        break;
+                    case 3:
+                        Null_Exception null_Exception = new Null_Exception();   
+                        null_Exception.Display();
+                        break;
+                    case 4:
+                        IndexOut_Of_Range_Exception indexOut_Of_Range= new IndexOut_Of_Range_Exception();
+                        indexOut_Of_Range.Range();
+                        break;
+
+
+                }
+            }
+           
         }
     }
 }
